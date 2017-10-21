22c4e2bffca45cb41c8793f18c0861c4ea5bc2ae
bleavjac <b.levajac@gmail.com>
2017-10-21 16:12:28 +0200

    Inicijalni commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      login: ['zpao', 'blevajac', 'gaearon']
+    }
+  }
+
+  render() {
+    return (
+      <div className="">
+        <p className="">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//css
+import './index.css';
+
+//component
+import App from './App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
